42b400d300dce2ff7d35f6c5a093fb3cf998fd55
milagrosmenendeztuja <143182953+milagrosmenendeztuja@users.noreply.github.com>
2023-11-14 13:01:41 -0300

    13:01 14/11

--- Proyecto/libreria/musculito.cpp
+++ Proyecto/libreria/musculito.cpp
@@ -3,19 +3,7 @@
 #include "musculito.h"
 
 eBusCliente BuscarCliente (std:: fstream& infileclientes, const sCliente ClienteBuscado )
-//eBusCliente BuscarCliente(sScliente *misCLiente, sCliente ClienteBuscado)
 {
-    /*for(unsigned long int i=0; i< sizeof(infileclientes);i++){
-        if (((infileclientes[i].nombre == ClienteBuscado.nombre) &&  (infileclientes[i].apellido==ClienteBuscado.apellido)) || (infileclientes[i].email == ClienteBuscado.email))
-        {
-            return eBusCliente::ExisteCliente;
-        }
-        if (i==sizeof(infileclientes))
-            break;
-
-        return eBusCliente::NoExisteCliente;
-        }
-*/
         infileclientes.seekg(0, std::ios::end);  // Mover el puntero al final del archivo
         std::streampos length = infileclientes.tellg();  // Obtener la posición actual del puntero (tamaño del archivo)
         infileclientes.seekg(0, std::ios::beg);  // Mover el puntero al principio del archivo
@@ -41,22 +29,7 @@ eBusCliente BuscarCliente (std:: fstream& infileclientes, const sCliente Cliente
     }
 
 int idCliente(std:: fstream& infileclientes, const sCliente ClienteBuscado)
-{
-       /* int BuscarCliente = BuscarCliente(infileclientes, ClienteBuscado);
-        if (BuscarClientes == ExisteCliente)
-        {
-            for(int i=0; i< sizeof(infileclientes);i++)
-            {
-                if (((infileclientes[i].nombre == ClienteBuscado.nombre) &&  (infileclientes[i].apellido==ClienteBuscado.apellido)) || (infileclientes[i].email == ClienteBuscado.email))
-                {
-                    return infileclientes[i].idCliente;
-                }
-            }
-        }
-        if (BuscarCliente == NoExisteCliente)
-        {
-            return 0;
-        }*/
+    {
         eBusCliente resultadoBusqueda = BuscarCliente(infileclientes, ClienteBuscado);
 
         if (resultadoBusqueda == eBusCliente::ExisteCliente) {
@@ -78,27 +51,6 @@ int idCliente(std:: fstream& infileclientes, const sCliente ClienteBuscado)
 
 eEstado estadoCuota (std:: fstream& infileclientes, sCliente ClienteBuscado)
 {
-       /* int posicion;
-
-        for(int i=0; i< sizeof(infileclientes);i++)
-        {
-            if(misClientes[i].idCliente == ClienteBuscado.idCliente)
-            {
-             posicion=i;
-            }
-        }
-     if(infileclientes[posicion].estado == 0)
-        {
-            return eEstado::alDia;
-        }
-     if(infileclientes[posicion].estado < 0)
-     {
-            return eEstado::Deuda;
-     }
-     if(infileclientes[posicion].estado > 0)
-     {
-            return eEstado::aFavor;
-     }*/
         sCliente clienteActual;
         int posicion = -1;
 
@@ -123,37 +75,6 @@ eEstado estadoCuota (std:: fstream& infileclientes, sCliente ClienteBuscado)
             return eEstado::alDia;
         }
 }
-
-/*eVerificarClase verificarClase (std:: fstream& infileclases,const sClases ClaseBuscada)
-{
-     int posicion;
-     for(int i=0; i<sizeof(infileclases); i++)
-     {
-            if(misClases[i].nombre == ClaseBuscada.nombre && infileclases[i].horario == ClaseBuscada.horario)
-            {
-             posicion = i;
-             return misClases[posicion].idClase;
-            }
-     }
-     return -1;
-
-        sClases claseActual;
-        int posicion = -1;
-
-        while (infileclases >> claseActual.nombre >> claseActual.horario >> claseActual.idClase) {
-            if (claseActual.nombre == ClaseBuscada.nombre && claseActual.horario == ClaseBuscada.horario) {
-                posicion = 0;
-                break;  // No necesitas seguir leyendo el archivo si encontraste la clase
-            }
-        }
-
-        if (posicion != -1) {
-            return claseActual.idClase;
-        } else {
-            // Clase no encontrada
-            return -1;
-        } //asad
-}*/
 
 int verificarClase(std::fstream& infileclases, const sClases& claseBuscada) {
         sClases claseActual;
@@ -210,96 +131,3 @@ eCupos verificarCupos()
 
 }
 
-eTipoClase nombreClase(std:: fstream& infileclases, int CupoMax)
-{
-
-        if (infileclases.nombre == "Spinning")
-        {
-            CupoMax = 45;
-            return eTipoClase::Spinning;
-        }
-        if (infileclases.nombre == "Yoga")
-        {
-            CupoMax = 25;
-            return eTipoClase::Yoga;
-        }
-        if (infileclases.nombre == "Pilates")
-        {
-            CupoMax = 15;
-            return eTipoClase::Pilates;
-        }
-        if (infileclases.nombre == "Stretching")
-        {
-            CupoMax = 40;
-            return eTipoClase::Stretching;
-        }
-        if (infileclases.nombre == "Zumba")
-        {
-            CupoMax = 50;
-            return eTipoClase::Zumba;
-        }
-        if (infileclases.nombre == "Boxeo")
-        {
-            CupoMax = 30;
-            return eTipoClase::Boxeo;
-        }
-        if (infileclases.nombre == "Musculacion")
-        {
-            CupoMax = 30;
-            return eTipoClase::Musculacion;
-        }
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*eRmCliente removerCliente (sCliente *misClientes, sCliente ClienteEliminado)
-{
-    sCliente *auxiliar = misClientes;
-    sCliente *ultimo = (gymMusculito -> misClientes) + gymMusculito -> CantClientes - 1;
-    while(true)
-    {
-        if ((auxiliar->nombre == ClienteEliminado.nombre && auxiliar->apellido == ClienteEliminado.apellido))
-        {
-            *auxiliar = ClienteNulo;
-            return eRmCliente::ExitoRemover;
-        }
-        if (auxiliar==ultimo)
-        {
-            break;
-        }
-        auxiliar++;
-    }
-    return eRmCliente::ErrRmNomApe;
-}*/
-
-/*eAgrCliente agregarCliente (sMusculito *gymMusculito, sCliente ClienteNuevo)
-{
-    if(!hayEspacio (gymMusculito))
-        return eAgrCliente::ErrAgrCliente;
-
-    gymMusculito->CantClientes++;
-    ClienteNuevo = gymMusculito->misClientes [gymMusculito->CantClientes-1];
-
-    return eAgrCliente ::ExitoAgrCliente;
-}*/
-/*
-int cantClientes (sCliente *misClientes)
-{
-
-}
-*/
