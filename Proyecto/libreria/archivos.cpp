bcc6f3d0188adf8e2eaebef1fcd45c9b9ebeedc3
milagrosmenendeztuja <143182953+milagrosmenendeztuja@users.noreply.github.com>
2023-11-14 14:26:40 -0300

    14:26 14/11

--- Proyecto/libreria/archivos.cpp
+++ Proyecto/libreria/archivos.cpp
@@ -1,7 +1,30 @@
 #include "archivos.h"
-/*
+
+str convertirString(float horario){
+    int horas = static_cast <int>(horario);  //obtengo la parte entera
+    int minutos = static_cast <int>((horario-horas)*60); //obtengo los minutos
+
+    ostringstream ss; //creo un stringstream para formatear el horario
+
+    if (horas < 10) {   //agrego las horas a la cadena
+        ss << "0";      //agrego un cero si la hora es menor que 10
+    }
+    ss << ":";
+    if(minutos < 10){   //agrego los minutos a la cadena
+        ss <<"0";       // agrego cero si los minutos son menos que 10
+    }
+    ss << minutos;
+
+    return ss.str();
+}
+
+string convertirIntAString(int estado) {
+    return to_string(estado);
+}
+
 void leerArchivoClientes (ifstream&infileclientes)
 {
+    /*
     //MANERA QUE NOS ENSEÑARON EN CLASES
      str header;
 
@@ -23,28 +46,50 @@ void leerArchivoClientes (ifstream&infileclientes)
         }
         cout << endl;
     }
-}
 */
 
-str convertirString(float horario){
-    int horas = static_cast <int>(horario);  //obtengo la parte entera
-    int minutos = static_cast <int>((horario-horas)*60); //obtengo los minutos
+    sCliente *misClientes = nullptr; //creo array dinamico
+    int numClientes = 0;
+    int i=0;
+    string n;
 
-    ostringstream ss; //creo un stringstream para formatear el horario
+    string linea;
+    while (getline(infileclientes, linea)){    //lee el archivo CSV line ppor linea
+        std :: istringstream ss(linea);
+        string campo;
 
-    if (horas < 10) {   //agrego las horas a la cadena
-        ss << "0";      //agrego un cero si la hora es menor que 10
+        sCliente cliente;                           //Divide la linea en campos utilizando comas como delimitadores
+        getline (ss, campo, ',');
+        cliente.idCliente = stoi(campo);
+        getline (ss, cliente.nombre, ',');
+        getline (ss, cliente.apellido, ',');
+        getline (ss, cliente.email, ',');
+        getline (ss, cliente.telefono, ',');
+        getline (ss, cliente.fechaNac, ',');
+        getline(ss, campo, ',');
+        cliente.estado = stoi(campo);
+
+
+        sCliente *tempArrayClientes = new sCliente[numClientes + 1];        // Aumenta el tamaño del array dinámico y agrega la clase
+        std::copy(misClientes, misClientes + numClientes, tempArrayClientes);
+        tempArrayClientes[numClientes] = cliente;
+
+        delete[] misClientes;
+        misClientes = tempArrayClientes;
+        ++numClientes;
+
     }
-    ss << ":";
-    if(minutos < 10){   //agrego los minutos a la cadena
-        ss <<"0";       // agrego cero si los minutos son menos que 10
-    }
-    ss << minutos;
+    infileclientes.close();   // Cierra el archivo CSV
 
-    return ss.str();
+    for (int i = 0; i < numClientes; ++i) {              // Ahora puedes trabajar con el array dinámico, por ejemplo, imprimir los datos
+
+        cout << "ID: " << misClientes[i].idCliente << ", Nombre: " << misClientes[i].nombre << ", Apellido: " << misClientes[i].apellido << ", Email: " << misClientes[i].email << ", Telefono: " << misClientes[i].telefono << ", Fecha de nacimiento: " << misClientes[i].fechaNac  << endl;
+    }
+
+    delete[] misClientes;   // Recuerda liberar la memoria cuando ya no la necesitas
+
+    return;
 }
-
-
 
 void leerArchivoClases (ifstream &infileclases)
 {
@@ -85,15 +130,15 @@ void leerArchivoClases (ifstream &infileclases)
         clase.idClase = stoi(campo);
         getline (ss, clase.nombre, ',');
         getline (ss, n, ',');
-        clase.horario[i] = stof(n);
+        clase.horario = stof(n);
 
 
-        sClases *tempArray = new sClases[numClases + 1];        // Aumenta el tamaño del array dinámico y agrega la clase
-        std::copy(misClases, misClases + numClases, tempArray);
-        tempArray[numClases] = clase;
+        sClases *tempArrayClases = new sClases[numClases + 1];        // Aumenta el tamaño del array dinámico y agrega la clase
+        std::copy(misClases, misClases + numClases, tempArrayClases);
+        tempArrayClases[numClases] = clase;
 
         delete[] misClases;
-        misClases = tempArray;
+        misClases = tempArrayClases;
         ++numClases;
 
     }
@@ -107,8 +152,6 @@ void leerArchivoClases (ifstream &infileclases)
     delete[] misClases;   // Recuerda liberar la memoria cuando ya no la necesitas
 
     return;
-
-
 }
 
 void leerArchivoAsistencias (ifstream infileasistencias)
